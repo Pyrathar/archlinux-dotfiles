b50894d431d3a8a7882c54f7ab8acab6e11571e8
Henrik Stanley Mortensen <dev@henrik.cloud>
2023-10-22 21:10:50 +0200

    feat: add eza to pwsh config

--- home/dot_config/powershell/executable_Microsoft.PowerShell_profile.ps1
+++ home/dot_config/powershell/executable_Microsoft.PowerShell_profile.ps1
@@ -16,14 +16,25 @@ else {
     Import-Module -Name Terminal-Icons
 }
 
+# Set FZF as the default PSReadline provider and reverse history.
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
-
 Set-PSReadLineOption -PredictionSource History
 
+# Load starship as shell prompt
 Invoke-Expression (&starship init powershell)
 
+# Load Zoxide and change default command to trigger from z to j
 Invoke-Expression (& {
     (zoxide init --hook pwd powershell --cmd j --no-aliases) -join "`n"
     })
 
 New-Alias j __zoxide_zi -Force
+
+# Overwrite ls alias to use eza
+Remove-Alias -Name ls
+Set-Alias -Name ls -Value Get-ChildItemEza
+Set-Alias -Name l -Value Get-ChildItemEza
+
+function Get-ChildItemEza {
+    eza --long --header --all --icons --hyperlink --sort type --git --group
+}
